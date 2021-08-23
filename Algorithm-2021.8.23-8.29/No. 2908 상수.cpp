4e019271a6e5c10f7520356c05575f8f23a3d827
Serendipity-Song <thdalwh3867@gmail.com>
2021-08-23 12:00:04 +0900

    상수

---
+++ Algorithm-2021.8.23-8.29/No. 2908 상수.cpp
@@ -0,0 +1,35 @@
+﻿
+
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+
+int main(){
+
+	int num1;
+	int num2;
+	int snum1 = 0;
+	int snum2=0;
+
+	cin >> num1 >> num2;
+
+
+		for (int i = 0; i < 3; i++) {
+
+		snum1[i] += num1 % 10;
+
+		}
+
+		for (int i = 0; i < 3; i++) {
+
+		snum2 += num2 % 10;
+
+		}
+
+
+		if (snum1 > snum2)
+			cout << snum1;
+		else cout << snum2;
+
+}
