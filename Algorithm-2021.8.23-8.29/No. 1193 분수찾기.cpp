f02c05b2a7fc80ebcb1232804d2d6ed27e3c3ad9
Serendipity-Song <thdalwh3867@gmail.com>
2021-08-26 18:07:29 +0900

    분수

---
+++ Algorithm-2021.8.23-8.29/No. 1193 분수찾기.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	int x; //순서
+	cin >> x;
+
+	//x가 5일때, 합이 4, 두번째
+	//2/4-2
+	//5가 왜 4인가
+	//1+2+3=6인데 3이상 6이하니까
+	//5-3=2   x-(1+2)=2, 
+
+	if (x = 1)
+		cout << 1;
+
+	else {
+		int sum;
+		for (int i = 1; i <= n; i++) {
+
+			for (int n = 1;sum<=n;n++) {
+				sum += n;
+			}
+
+		}
+	}
+
+	for(int i=1;)
+	하나씩1 + 2 + 3 + 4 더하자;
+
+	더한 횟수가 갯수이고;
+	더한횟수 + 1이 각 자리 합;
+
+
+
+}
