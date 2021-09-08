701b115deff1883d7064aabb87cd9cf94a79d121
Serendipity-Song <thdalwh3867@gmail.com>
2021-09-08 14:21:02 +0900

    수 정렬하기

--- Algorithm-2021.8.23-8.29/No. 2750 수 정렬하기 .cpp
+++ Algorithm-2021.8.23-8.29/No. 2750 수 정렬하기 .cpp
@@ -1,12 +1,22 @@
 
-using namespace std;
+
 #include <iostream>
-
-
-
+using namespace std;
 int main() {
-	//중복을 제거하기
-	//오름차순으로
-
-
+    int num;
+    int arr[1000] = { 0, };
+    int tmp;
+    cin >> num;
+    for (int i = 0; i < num; i++)
+        cin >> arr[i];
+    for (int i = num; i > 1; i--)
+        for (int j = 0; j + 1 < i; j++)
+            if (arr[j] > arr[j + 1])
+            {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+    for (int i = 0; i < num; i++)
+        cout << arr[i] << '\n';
 }
